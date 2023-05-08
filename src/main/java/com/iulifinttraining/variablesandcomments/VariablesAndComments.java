54e0eebeff7d068c4f9cb66007f5df0cde209b6a
iulik98 <fintinaiulian6@gmail.com>
2023-05-08 20:16:29 +0300

    Added the Variables and Comments Task

---
+++ src/main/java/com/iulifinttraining/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,47 @@
+package com.iulifinttraining.variablesandcomments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+        // Declare the primitive data type variables
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowestLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowestLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowestLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowestLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+        falseBooleanVariable= false;
+        trueBooleanVariable=true;
+        firstCharFromAscii='a';
+        lastCharFromAscii='Z';
+        lowerLimitOfByteDataType=-128;
+        upperLimitOfByteDataType=127;
+        lowerLimitOfShortDataType=-32768;
+        upperLimitOfShortDataType=32767;
+        lowestLimitOfIntDataType=-2147483648;
+        upperLimitOfIntDataType=2147483647;
+        lowestLimitOfLongDataType=-9223372036854775808L;
+        upperLimitOfLongDataType=9223372036854775807L;
+        lowestLimitOfFloatDataType=-3.4E+38f;
+        upperLimitOfFloatDataType=3.4E+38f;
+        lowestLimitOfDoubleDataType=-1.7E+308d;
+        upperLimitOfDoubleDataType=1.7E+308d;
+
+        System.out.println(lowerLimitOfShortDataType);
+
+
+
+    }
+}
