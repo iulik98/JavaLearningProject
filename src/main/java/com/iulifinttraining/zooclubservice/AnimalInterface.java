5eee26f6bf789778e6084fb85f34cb2f9227e1e9
iulik98 <fintinaiulian6@gmail.com>
2023-05-25 09:35:21 +0300

    Added the implementation of ZOO Service by using OOP

---
+++ src/main/java/com/iulifinttraining/zooclubservice/AnimalInterface.java
@@ -0,0 +1,7 @@
+package com.iulifinttraining.zooclubservice;
+
+public interface AnimalInterface {
+
+    //Metoda makeSound() trebuie sa fie implementata in toate clasele care vor implementa interfata AnimalInterface
+    void makeSound();
+}
